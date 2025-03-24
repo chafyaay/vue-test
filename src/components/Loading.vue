0d29c0d68cb2322a6cee1ea45b43c16d87926b1f
yassine chafyaay <yassine.chafyaay@bp.com>
2025-03-24 15:42:56 +0000

    implemented loading components

---
+++ src/components/Loading.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class="loading">
+      <img class="img" :src="loadingImg" alt="Loading..." />
+    </div>
+  </template>
+  
+  <script setup>
+  import loadingImg from "../assets/loading.svg";
+  </script>
+  
+  <style scoped>
+  .loading {
+    display: flex;
+    justify-content: center; /* Centers the image horizontally */
+    align-items: center;    /* Centers the image vertically */
+    width: 100%;
+    height: 3.5rem;
+  }
+  
+  .img {
+    height: 100%;
+    animation: rotate 500ms linear infinite;
+    opacity: 0.3;
+  }
+  
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  </style>
