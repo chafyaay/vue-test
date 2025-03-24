f5cf42bd0afcfce681b8f5818410fea7bc6874ae
yassine chafyaay <yassine.chafyaay@bp.com>
2025-03-24 15:42:20 +0000

    implemented pagination card components

---
+++ src/components/Pagination.vue
@@ -0,0 +1,27 @@
+ <template>
+<span> Show </span>
+<select v-model="nbrCardsPerPage" @change="changeHandler">
+    <option value="10">10</option>
+    <option value="20">20</option>
+    <option value="50">50</option>
+</select>
+<span> elements / Page </span>
+</template>
+
+<script setup>
+  import { defineModel } from 'vue';
+  const nbrCardsPerPage=defineModel()
+</script>
+
+<style scoped>
+    select{
+        height: 2rem;
+        border: 1px solid #CCC;
+        border-radius: .3rem;
+        width: 3rem;
+        color:  #333;
+    }
+    span{
+      font-size: .81rem;
+    }
+</style> 
